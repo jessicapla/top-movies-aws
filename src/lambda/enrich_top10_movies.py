607fdbcbfe3e42ffaa6aa0c66a3fd0a90b1c5a39
Jessica Pla <jessicab.pla@gmail.com>
2025-07-05 15:08:15 -0300

    Add enrich_top10_movies lambda handler

---
+++ src/lambda/enrich_top10_movies.py
@@ -0,0 +1,21 @@
+import requests
+import logging
+from typing import Dict, Any, List
+
+logger = logging.getLogger()
+logger.setLevel("INFO")
+
+def enrich_top10_movies() -> None:
+  """Placeholder"""
+
+
+
+def lambda_handler(event: Dict[str, Any], context: Any) -> None:
+  """
+  Main Lambda handler function
+  Parameters:
+      event: Dict containing the Lambda function event data
+      context: Lambda runtime context
+  Returns:
+      TODO
+  """
